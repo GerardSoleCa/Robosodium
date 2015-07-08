ffb0880a59e53bb6deee9083a87f5ffe695bc56c
Gerard Solé <gerard.sole@be12.ch>
2015-07-08 16:26:57 +0200

    Adding jni linux script

--- build_jni_linux.sh
+++ build_jni_linux.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+rm -rf libsodium
+git submodule init
+git submodule update
 
 echo "Creating linux_lib directory"
 mkdir linux_lib
 cd libsodium
 
 echo "Configuring prefix"
+./autogen
 ./configure --prefix=`pwd`/../linux_lib
 make && make check && make install
 
@@ -12,8 +16,7 @@ cd ..
 cd jni
 ./installswig.sh
 
-export JAVA_HOME=/usr/bin/
-./compile
+./compile.sh "linux"
 
 echo "Cleaning libsodium changes"
 cd ..
