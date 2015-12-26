83e06f80baa1fea44e66c75d610153ee36244f6f
Benedict Lau <ben.hy.lau@gmail.com>
2015-12-26 10:13:20 +0100

    Fix AAR build scripts

--- do_the_job.sh
+++ do_the_job.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "Argument: $1\n\n"
-#./install_software.sh
+./install_software.sh
 if [ -n "$1" ] && [ "$1" = "linux" ]; then
     ./build_jni_linux.sh
 else
-#    ./download_ndk.sh
+    ./download_ndk.sh
     ./build_android_libsodium.sh
     ./build_jni.sh
 fi
