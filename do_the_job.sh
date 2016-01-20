e528c75ac5189843826c5cdfa310fa0c00a7532c
Can Yumusak <can.yumusak@quartett-mobile.de>
2016-01-20 17:30:18 +0100

    Added switch between osx and win

--- do_the_job.sh
+++ do_the_job.sh
@@ -3,8 +3,15 @@
 echo "Argument: $1\n\n"
 ./install_software.sh
 if [ -n "$1" ] && [ "$1" = "linux" ]; then
+	echo "Building on Linux"
     ./build_jni_linux.sh
+else if [ -n "$1" ] && [ "$1" = "osx" ]; then
+	echo "Building on OSx"
+	./download_ndk_osx.sh
+    ./build_android_libsodium_osx.sh
+    ./build_jni_osx.sh
 else
+	echo "Building on Windows"
     ./download_ndk.sh
     ./build_android_libsodium.sh
     ./build_jni.sh
